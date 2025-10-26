ce931c818ce7c742421466cb067be98aba75e7f1
Piotr Zbigniew Hic <piotrhic@gmail.com>
2025-10-26 12:50:57 +0100

    Main Class fix

--- src/main/java/core/basesyntax/Main.java
+++ src/main/java/core/basesyntax/Main.java
@@ -8,7 +8,7 @@ public class Main {
         SalaryInfo salaryInfo = new SalaryInfo();
         String[] names = new String[] {"John", "Andrew", "Kate"};
         String[] dates = new String[] {"date from = `01.04.2019`", "date to = `30.04.2019`"};
-        String[] data = new String[] {"26.04.2019 John 4 50", "05.04.2019 Andrew 3 200"
+        String[] data = new String[] {"26.04.2019 John 4 50", "05.04.2019 Andrew 3 200",
                 "10.04.2019 John 7 100", "22.04.2019 Kate 9 100", "25.06.2019 John 11 50",
                 "26.04.2019 Andrew 3 150", "13.02.2019 John 7 100", "26.04.2019 Kate 9 100}"};
         System.out.println(salaryInfo.getSalaryInfo(names,data,firstDate,secondDate));
